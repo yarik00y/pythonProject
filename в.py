c247082d3415938fd2d7bd1e02a5a93c084870e9
yarik <super_yarik00@ukr.net>
2023-04-01 13:43:58 +0300

    3

---
+++ в.py
@@ -0,0 +1,14 @@
+def checker(function):
+    def checker(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+        except Exception as exc:
+            print(f"We have problems {exc}")
+        else:
+            print(f"No problems. Result – {result}")
+    return checker
+
+@checker
+def calculate(expression):
+    return eval(expression)
+calculate("2+2")
